ee23b4eb5aea8856c40312bb8d668a3de5801845
Joey Lee <joeylleyi@u.nus.edu>
2024-06-23 19:20:26 +0700

    feat: photo page

--- src/app/(pages)/(landing)/photo-journal/page.tsx
+++ src/app/(pages)/(landing)/photo-journal/page.tsx
@@ -1,5 +1,32 @@
+import { Typography } from "@/components/typography";
 import React from "react";
 
-export default function page() {
-  return <div>page</div>;
+export default function PhotoJournalPage() {
+  return (
+    <div className="grid justify-items-center">
+      <div className="flex items-center flex-col max-w-4xl space-y-20">
+        <Typography variant={"headline-md"} weight={"semibold"}>
+          my photo gallery {"<3"}
+        </Typography>
+        <div className="grid grid-cols-3">
+          <div className="cols-span-1 w-full space-y-4 px-4">
+            <img src="/mochi.png" className="rounded-xl max-w-full" />
+            <img src="/mochi.png" className="rounded-xl max-w-full" />
+            <img
+              src="/british-museum-pic.jpg"
+              className="rounded-xl max-w-full"
+            />
+          </div>
+          <div className="cols-span-1 w-full space-y-4 px-4">
+            <img src="/british-museum-pic.jpg" className="rounded-xl" />
+            <img src="/mochi.png" className="rounded-xl" />
+          </div>
+          <div className="cols-span-1 w-full space-y-4 px-4">
+            <img src="/mochi.png" className="rounded-xl" />
+            <img src="/british-museum-pic.jpg" className="rounded-xl" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
